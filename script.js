69e356ed73dc020d6c702395bc45a2677e8439b5
Aryan Singhal <115396133+AryanSinghal17@users.noreply.github.com>
2026-09-04 23:09:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 function makeid(l) {
   // write your code here
+	let res = "";
+	let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	
+	str += str.toLowerCase();
+	str+ = "123456789";
+
+	for(int i = 0;i<l;i++){
+		let idx = Math.floor(Math.random() * str.length);
+		res += str[idx];
+	}
+
+	return res;
 }
 
 // Do not change the code below.
-const l = prompt("Enter a number.");
+// const l = prompt("Enter a number.");
 alert(makeid(l));
